2feb5840b072b346021a6d65365c2454b6784d77
eonderko <eonderko@hawaii.edu>
2020-02-11 10:33:14 -1000

    completed parts 1-9

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstTabLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    self.firstTabLabel.text = "About"
+        
     }
 
 
